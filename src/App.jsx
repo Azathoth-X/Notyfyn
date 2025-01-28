9643b0e6c06d4af21e9d763a9b2a43260102954b
Azathoth-X <ranaprikshit25@gmail.com>
2025-01-28 22:36:48 +0530

    style: adjust layout and spacing in App and Hero components

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="flex flex-col h-screen">
       <Navbar/>
-      <main className="flex-1 flex flex-row justify-around items-center">
+      <main className="flex-1 flex flex-col md:flex-row justify-around items-center">
         <div>
           <Hero/>
         </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@
 const Hero = ()=> {
     return (
         <div>
-            <div className="flex flex-col p-20 items-center text-white text-center">
+            <div className="flex flex-col  p-10 md:p-20 items-center text-white text-center">
                 <div>
                     <h1 className="font-extrabold font-mono  text-8xl">NOTYFYN</h1>
                 </div>
