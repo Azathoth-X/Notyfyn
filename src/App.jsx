3fb136d1250713bdcedcd70054ed0c288973391f
Azathoth-X <ranaprikshit25@gmail.com>
2025-01-28 22:31:32 +0530

    refactor: update favicon, change title, and restructure app layout with navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import './App.css'
 import Hero from './components/Hero'
 import Logic from './components/Logic'
+import Navbar from './components/navbar'
 
 function App() {
-
   return (
-    <div className='flex flex-row justify-around items-center min-h-screen '>
-      <div >
-        <Hero/>
-      </div>
-      <div>
-        <Logic/>
-      </div>
+    <div className="flex flex-col h-screen">
+      <Navbar/>
+      <main className="flex-1 flex flex-row justify-around items-center">
+        <div>
+          <Hero/>
+        </div>
+        <div>
+          <Logic/>
+        </div>
+      </main>
     </div>
-    
   )
 }
 
